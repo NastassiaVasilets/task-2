594a76fde4834842d06b0ec1399e2e764070b985
NastassiaVasilets <Nastena_vg@mail.ru>
2016-03-12 23:01:09 +0300

    ready for SHRI

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 (function (root) {
-    var map = root.maze.MAZE_Y;
+    var map = root.maze.MAZE_31;
     var path = root.maze.solution(map, 1, 0);
 
-    document.querySelector('.outer').appendChild(
-        root.maze.render(map, path)
-    );
+	var tempPath = [],
+    	i = 0;
+	setInterval(function(){
+    		if (i < path.length) document.querySelector('.outer').innerHTML = "";
+    		if (i < path.length){
+    	tempPath[i] = path[i];
+    	document.querySelector('.outer').appendChild(
+        	root.maze.render(map, tempPath)
+    	);
+    	i++;
+    	}
+    },1);
 })(this);
--- js/solution.js
+++ js/solution.js
@@ -14,10 +14,114 @@
      */
     function solution(maze, x, y) {
         // todo: построить правильный маршрут к выходу
-        return [
-            [1, 0],
-            [1, 1]
-        ];
+        var p = [], 
+            point,
+            i = 1,
+            flag = "d",
+            current = [x,y];
+        p[0] = [x,y];
+        while(maze[y + 1][x] == 0){
+            current = fun3(current);
+        }
+        while (y < maze.length - 1){
+        switch (flag){
+            case "d":{
+                if (maze[y][x - 1] == 0){
+                    current = fun1(current);
+                    flag = "l";
+                } else if(maze[y + 1][x] == 0){
+                    current = fun3(current);
+                }
+                else  if (maze[y][x + 1] == 0){
+                    current = fun2(current);
+                    flag = "r";
+                } else {
+                    current = fun4(current);
+                    flag = "u";
+                }
+                break;
+            }
+            case "r":{
+                if (maze[y + 1][x] == 0){
+                    current = fun3(current);
+                    flag = "d";
+                } else if(maze[y][x + 1] == 0){
+                    current = fun2(current);
+                }else if (maze[y - 1][x] == 0){
+                    current = fun4(current);
+                    flag = "u";
+                } else {
+                    current = fun1(current);
+                    flag = "l";
+                }
+                break;
+            }
+            case "l":{
+                if (maze[y - 1][x] == 0){
+                    current = fun4(current);
+                    flag = "u";
+                } else if(maze[y][x - 1] == 0){
+                    current = fun1(current);
+                }else if (maze[y + 1][x] == 0){
+                    current = fun3(current);
+                    flag = "d";
+                } else {
+                    current = fun2(current);
+                    flag = "r";
+                }
+                break;
+            }
+            case "u": {
+                if (maze[y][x + 1] == 0){
+                    fun2(current);
+                    flag = "r";
+                } else if(maze[y - 1][x] == 0){
+                    fun4(current);
+                }else if (maze[y][x - 1] == 0){
+                    fun1(current);
+                    flag = "l";
+                } else {
+                    fun3(current);
+                    flag = "d";
+                }
+                break;
+            }
+            default: break;
+        }
+        }
+        function fun1(current){
+            point = [x - 1,y];
+            p[i] = point;
+            i++;
+            x--;
+            current = [x,y];
+            return current;
+        }
+        function fun2(current){
+            point = [x + 1,y];
+            p[i] = point;
+            i++;
+            x++;
+            current = [x,y];
+            return current;
+        }
+        function fun3(current){
+            point = [x,y + 1];
+            p[i] = point;
+            i++;
+            y++;
+            current = [x,y];
+            return current;
+        }
+        function fun4(current){
+            point = [x, y - 1];
+            p[i] = point;
+            i++;
+            y--;
+            current = [x,y];
+            return current;
+        }
+        return p;
     }
 
     root.maze.solution = solution;
